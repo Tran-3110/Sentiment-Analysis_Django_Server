ce303f36623e0f6952e830c91ca3b78bd40fb8bc
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2024-12-15 14:29:54 +0700

    Đổi port trong django

--- manage.py
+++ manage.py
@@ -15,6 +15,10 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    if len(sys.argv) == 1:
+        sys.argv += ["runserver", "127.0.0.1:3110"]  # Lệnh runserver và địa chỉ + port
+    elif sys.argv[1] == "runserver" and len(sys.argv) == 2:
+        sys.argv += ["127.0.0.1:3110"]  # Chỉ thêm port nếu chưa có
     execute_from_command_line(sys.argv)
 
 
