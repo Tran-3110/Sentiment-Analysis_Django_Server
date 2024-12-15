1578dff8b0dada91a5652c23f225f250fd27c5fb
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2024-12-15 17:15:38 +0700

    Thêm đường dẫn tạm thời cho rest api

--- sentiment_analysis_server/urls.py
+++ sentiment_analysis_server/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from os import path
+
 from django.contrib import admin
-from django.urls import path
+# from web_server_application.views import SentimentAnalysisView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('setiment-analysis/', SentimentAnalysisView.as_view(), name='setiment-analysis'),
+
 ]
